971cfc60d5bc3e5dbe0e07c9e6e51745b390a158
Jerome Hontiveros <jrmhntvrs@gmail.com>
2025-12-07 19:36:56 +0800

    api: profile role check

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,6 +69,17 @@ class User extends Authenticatable
     }
 
     /**
+     * Check if the user has a specific role.
+     *
+     * @param string $role
+     * @return bool
+     */
+    public function hasRole(string $role): bool
+    {
+        return $this->roles->contains('name', $role);
+    }
+
+    /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
