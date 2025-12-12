61ec3bcc58db65a696ad52ea1891ef782bee6ae8
samanthaheartmatiga <matigasamanthaheart@gmail.com>
2025-12-12 12:56:04 +0800

    Header and Sidebar

--- resources/js/components/layout/app_navbar.vue
+++ resources/js/components/layout/app_navbar.vue
@@ -1,103 +1,30 @@
-<!--
- * System Name: Human Resource Management Information System
- * Module Name: None
- * Purpose Of this file: 
- * To provide a responsive navigation bar with theme switching and user menu.
- * 
- * Author: Jerome Andrei O. Hontiveros
- * Copyright (C) 2025
- * by the Department of Science and Technology — Project LODI
- * All rights reserved.
- * 
- * Permission is hereby granted, free of charge, to any persons obtaining a copy
- * of this software and associated documentation files, to deal in the Software
- * without restriction, including the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, provided that the
- * above copyright notice(s) and this permission notice appears in all copies of
- * the Software and that both the above copyright notice(s) and this permission
- * notice appear in supporting documentation.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS INCLUDED IN THIS NOTICE BE
- * LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY
- * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- * 
- * Except as contained in this notice, the name of a copyright holder shall not
- * be used in advertising or otherwise to promote the sale, use or other dealings
- * in this Software without prior written authorization of the copyright holder.
--->
 <template>
-  <!-- Main navigation bar container -->
-  <header class="navbar">
-    <div class="navbar-content">
-      <!-- Left section -->
-      <div class="navbar-left">
-        <!-- Button to toggle the sidebar visibility (only show when sidebar is enabled) -->
-        <button v-if="layoutStore.layoutPreference !== 'navbar'"
-                @click="$emit('toggle-sidebar')" 
-                class="menu-button"
-                aria-label="Toggle sidebar menu">
-          <IconifyIcon icon="mdi:menu" class="menu-icon" aria-hidden="true" />
-        </button>
-        
-        <!-- Dynamic page title that shows current route information -->
-        <h1 class="page-title">
-          <span class="prefix">Website Template | </span>
-          <span class="page-name">{{ currentPageTitle }}</span>
-        </h1>
-      </div>
-
-      <!-- Center navigation - only show when not in 'both' layout mode -->
-      <nav v-if="layoutStore.layoutPreference !== 'both'" class="navbar-nav" aria-label="Primary">
-        <ul role="menu">
-          <li v-for="item in navItems" 
-              :key="item.path" 
-              role="none">
-            <router-link :to="item.path" 
-                        class="nav-link" 
-                        :title="item.text"
-                        role="menuitem"
-                        :aria-current="$route.path === item.path ? 'page' : undefined">
-              <span class="icon" aria-hidden="true">
-                <IconifyIcon :icon="item.icon" width="20" height="20" />
-              </span>
-              <span class="text">{{ item.text }}</span>
-            </router-link>
-          </li>
-        </ul>
-      </nav>
-
-      <!-- Right section -->
-      <div class="navbar-right">
-        <!-- Theme selector with icon and dropdown -->
-        <div class="theme-selector">
-          <IconifyIcon :icon="currentThemeIcon" class="theme-icon" />
-          <span class="theme-name">
-            {{ currentThemeName }}
-          </span>
-          <IconifyIcon icon="mdi:chevron-down" class="dropdown-arrow" />
-          <select 
-            v-model="currentTheme" 
-            class="theme-select"
-          >
-            <option 
-              v-for="theme in availableThemes" 
-              :key="theme.id" 
-              :value="theme.id"
-            >
-              {{ theme.name }}
-            </option>
-          </select>
+  <header class="pup-navbar">
+    <div class="navbar-container">
+      
+      <div class="navbar-branding">
+        <div class="logo-circle">
+          <span class="logo-text">LOGO</span> 
         </div>
 
-        <!-- User menu section -->
+        <div class="university-info">
+          <h1 class="uni-name">Polytechnic University of the Philippines</h1>
+          <span class="system-name">T-SIS</span>
+        </div>
+      </div>
+
+      <div class="navbar-actions">
+        
+        <nav class="top-links">
+          <a href="#" class="header-link">HOMEPAGE</a>
+          <span class="separator">|</span>
+          <a href="#" class="header-link">COURSE</a>
+        </nav>
+
         <div class="user-menu" @click.stop="toggleUserMenu">
-          <button class="user-button" aria-label="User menu" aria-expanded="isUserMenuOpen">
+          <button class="user-button" aria-label="User menu" :aria-expanded="isUserMenuOpen">
+            <span class="user-label-text">USER</span>
+            
             <div class="user-avatar-container">
               <template v-if="userStore.user?.avatar">
                 <img :src="userStore.user.avatar" :alt="userStore.user.name || 'User'" class="user-avatar" />
